95c74b6ab4db99f0354f3216beb55d585c316b27
5tka <ferol2010@yandex.ru>
2017-02-18 12:45:39 +0200

    main

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,48 @@
 console.log('hello from script.js');
+$(document).ready(function(){
+
+    $('.bestsellerItems-slider').bxSlider({
+        slideWidth: 205,
+        // slideWidth: 194,
+        // infiniteLoop: false,
+        // slideMargin: 15,
+        pager:!1,
+        nextText:"",
+        prevText:"",
+        minSlides:1,
+        maxSlides:4,
+        moveSlides:1,
+    });
+    var all = $('.double__slider-wrap .title') // клас стиля ссылки
+	all.click( function(){
+		all.removeClass('title--active')   // клас активной ссылки
+		$(this).addClass('title--active')
+	})
+    $('.slide-ev2').parent().parent().addClass('dn');
+    $('.title-ev1').click(function(){
+        $('.slide-ev2')
+            .parent()
+            .parent()
+            .removeClass('db')
+            .addClass('dn');
+  
+        $('.slide-ev1')
+            .parent()
+            .parent()
+            .removeClass('dn')
+            .addClass('db');
+    });
+    $('.title-ev2').click(function(){
+        $('.slide-ev2')
+            .parent()
+            .parent()
+            .removeClass('dn')
+            .addClass('db');
+  
+        $('.slide-ev1')
+            .parent()
+            .parent()
+            .removeClass('db')
+            .addClass('dn');
+    });
+});
