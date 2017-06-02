2e99fab1431a4a2f6c37614df5689e183efca566
5tka <ferol2010@yandex.ru>
2017-06-02 12:08:15 +0300

    fin

--- src/js/vendor/vendor.js
+++ src/js/vendor/vendor.js
@@ -1,7 +1,7 @@
 console.log('hello from vendor.js');
 //="../../../bower_components/jquery/dist/jquery.js"
 //="../../../bower_components/bPopup/jquery.bpopup.js"
-//
+//="../../../bower_components/lightbox2/src/js/lightbox.js"
 /*!
  * jQuery Raty FA - A Star Rating Plugin with Font Awesome
  *
